a81ec22f36b9ecde6614e30ca961e073c3b23db9
jmlopez-technisys <jmlopez@technisys.com>
2017-05-17 15:28:56 -0300

    fruta

--- src/main/java/com/example/WebAppApplication.java
+++ src/main/java/com/example/WebAppApplication.java
@@ -3,11 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import user.UserController;
+
 
 @SpringBootApplication
 @ComponentScan({"com.example" , "user"})
+//@ComponentScan(basePackageClasses = UserController.class)
 public class WebAppApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
+		
+		
 	}
 }
--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -12,8 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
+        http
+        	.authorizeRequests().antMatchers("/").permitAll().and()
+            .authorizeRequests().antMatchers("/console/**").permitAll()
             	.antMatchers("/user/*").permitAll()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
@@ -24,6 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Autowired
